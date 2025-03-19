32a1ac810f1bb860ca55551bd5f3fd2bb517c03b
tyai <kyokosato3@outlook.jp>
2025-03-19 09:29:03 +0900

    チャイ_task_1 githubの使い方確認

--- src/font.ts
+++ src/font.ts
@@ -1,4 +1,4 @@
-import { Noto_Sans_JP, Dela_Gothic_One } from "next/font/google";
+import { Noto_Sans_JP, Dela_Gothic_One, Kiwi_Maru } from "next/font/google";
 
 export const notojp = Noto_Sans_JP({
   weight: ["400", "700"],
@@ -8,6 +8,12 @@ export const notojp = Noto_Sans_JP({
 
 export const delagothicone = Dela_Gothic_One({
   weight: ["400"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const kiwimaru = Kiwi_Maru({
+  weight: ["500"],
   subsets: ["latin"],
   display: "swap",
 });
