2a2666785c5304db93cc37da81766325915f92da
teteen <jiatengren92@gmail.com>
2025-03-20 21:40:10 +0900

    鉄チンok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,9 @@ export default function Home() {
         <p className={`${notojp.className} text-left font-bold text-5xl w-40`}>ピスラ</p>
         <p className={`${delagothicone.className} text-center text-4xl`}>OK</p>
       </div>
-      <div className="flex space-x-20 items-center px-10 py-5">
+      <div className="flex space-x-20 items-center px-10 py-5 tracking-wide uppercase text-white bg-blue-700">
         <p className={`text-left font-bold text-5xl w-40`}>鉄チン</p>
-        <p className={`text-center text-4xl`}></p>
+        <p className={`text-center text-4xl font-extrabold`}>ok</p>
       </div>
       <div className="flex space-x-20 items-center px-10 py-5 shadow-xl bg-gradient-to-r from-cyan-200 to-purple-200">
         <p className={`${kiwimaru.className} text-left text-5xl w-40`}>チャイ</p>
