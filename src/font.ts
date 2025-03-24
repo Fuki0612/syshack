65b5e7b834a10bf802f41fa6a66ff667625b2289
tyai <kyokosato3@outlook.jp>
2025-03-25 00:11:16 +0900

    tyai_task_3

--- src/font.ts
+++ src/font.ts
@@ -1,4 +1,4 @@
-import { Noto_Sans_JP, Dela_Gothic_One, Kiwi_Maru } from "next/font/google";
+import { Noto_Sans_JP, Dela_Gothic_One, Kiwi_Maru, Zen_Kaku_Gothic_New } from "next/font/google";
 
 export const notojp = Noto_Sans_JP({
   weight: ["400", "700"],
@@ -15,5 +15,11 @@ export const delagothicone = Dela_Gothic_One({
 export const kiwimaru = Kiwi_Maru({
   weight: ["500"],
   subsets: ["latin"],
+  display: "swap",
+});
+
+export const zenkakugothicnew = Zen_Kaku_Gothic_New({
+  weight: ["500"],
+  subsets: ["latin-ext"],
   display: "swap",
 });
